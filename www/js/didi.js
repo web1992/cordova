222a79bd13cc5bb171ccf2e8370644154865d495
web1992 <554933654@qq.com>
2016-10-20 11:31:21 +0800

    fix didi param

--- www/js/didi.js
+++ www/js/didi.js
@@ -15,7 +15,10 @@ var didiJK=null
 
     $('#opendidi').click(function () {
         console.log('didiJK click'+didiJK);
-        didiJK.didiPluginFun();
+	//var str="{fromlat:\"121.487872\",fromlng;\"31.174141\",tolat:\"121.487872\",tolng:\"31.174141\",biz:\"2\"}";
+	var str="[\"39.976694\",\"116.306169\",\"39.976694\",\"116.306169\",\"2\"]";
+        didiJK.didiPluginFun(str);
+        //didiJK.didiPluginFun();
     })
 
 });
